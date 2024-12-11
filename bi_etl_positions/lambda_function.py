c36216a15eced8426782e5631017002f8742a030
nikhilkudavemnh <114910344+nikhilkudavemnh@users.noreply.github.com>
2024-12-11 22:53:17 +0530

    update

--- bi_etl_positions/lambda_function.py
+++ bi_etl_positions/lambda_function.py
@@ -6,15 +6,16 @@ import time
 def lambda_handler(event: dict, context: dict):
     logger.info(f"Received event: {event}")
     start_time = time.time()
-    src_schema_name = event.get('scrSchemaName')
-    dst_schema_name = event.get('dstSchemaName')
-    client_id = event.get('clientId')
+    src_schema_name = event.get('schema_name')
+    dst_schema_name = event.get('schema_name')
+    client_id = event.get('client_id')
     try:
         results = etl_runner(src_schema_name, dst_schema_name)
         return {
             "statusCode": 200,
             "jobStatus": "Success",
             "clientId": client_id,
+            "clientName": event.get('client_name'),
             "completedTime": str(time.time()),
             "schemaName": src_schema_name,
             "timeTaken": time.time() - start_time,
@@ -26,6 +27,7 @@ def lambda_handler(event: dict, context: dict):
             "jobStatus": "Failed",
             "message": str(e),
             "clientId": client_id,
+            "clientName": event.get('client_name'),
             "completedTime": time.time() - start_time,
             "schemaName": src_schema_name
         }
